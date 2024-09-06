737fff78851c29952a4f6340005bbee43adbb704
GibbedHead <chaplygin-ma@yandex.ru>
2024-09-06 13:46:58 +0300

    Заготовка под реализацию всех переопределенных методов интерфейсов

---
+++ src/main/java/ru/chaplyginma/MyArrayListDemo.java
@@ -0,0 +1,14 @@
+package ru.chaplyginma;
+
+import ru.chaplyginma.list.MyArrayList;
+
+import java.util.List;
+
+public class MyArrayListDemo {
+
+    public static void main(String[] args) {
+        List<String> list = new MyArrayList<>();
+        System.out.println(list.size());
+
+    }
+}
