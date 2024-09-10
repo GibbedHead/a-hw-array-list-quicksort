a65fecea4ed7c8bcc7ec790979b6210f19b0994b
GibbedHead <chaplygin-ma@yandex.ru>
2024-09-10 16:03:24 +0300

    JavaDoc для демо класса быстрой сортировки
    Took 5 minutes

--- src/main/java/ru/chaplyginma/sort/MyListSort.java
+++ src/main/java/ru/chaplyginma/sort/MyListSort.java
@@ -4,12 +4,39 @@ import ru.chaplyginma.list.MyArrayList;
 
 import java.util.Comparator;
 
+/**
+ * Provides static methods for sorting instances of {@link MyArrayList} using
+ * the QuickSort algorithm.
+ *
+ * <p>The QuickSort algorithm is a recursive divide-and-conquer algorithm. It works
+ * by selecting a pivot element from the array and partitioning the array
+ * around the pivot such that all elements smaller than the pivot are placed
+ * before it, and all elements greater than the pivot are placed after it.
+ * This process is then recursively applied to the subarrays before and after
+ * the pivot until the entire array is sorted.
+ */
 public class MyListSort {
 
+    /**
+     * Sorts the given {@link MyArrayList} using the QuickSort algorithm
+     * with a custom {@link Comparator} to define the ordering.
+     *
+     * @param list       The {@link MyArrayList} to be sorted.
+     * @param comparator The {@link Comparator} to be used for comparing
+     *                   elements.
+     * @param <T>        The type of elements in the list.
+     */
     public static <T> void quickSort(MyArrayList<T> list, Comparator<T> comparator) {
         qSort(list, comparator, 0, list.size() - 1);
     }
 
+    /**
+     * Sorts the given {@link MyArrayList} using the QuickSort algorithm
+     * using the natural ordering of the elements (using {@link Comparable}).
+     *
+     * @param list The {@link MyArrayList} to be sorted.
+     * @param <T>  The type of elements in the list.
+     */
     public static <T extends Comparable<T>> void quickSort(MyArrayList<T> list) {
         qSort(list, Comparator.naturalOrder(), 0, list.size() - 1);
     }
